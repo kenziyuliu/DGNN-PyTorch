4df272fd5f433e7c4279130e73e2df1f49574e40
Ken Liu <liuziyu0816@hotmail.com>
2019-07-13 13:47:45 +1000

    Added some comments

--- data_gen/gen_bone_data.py
+++ data_gen/gen_bone_data.py
@@ -2,49 +2,53 @@ import os
 import numpy as np
 from numpy.lib.format import open_memmap
 
-paris = {
-    'ntu/xview': (
-        (1, 2), (2, 21), (3, 21), (4, 3), (5, 21), (6, 5), (7, 6), (8, 7), (9, 21), (10, 9), (11, 10), (12, 11),
-        (13, 1),
-        (14, 13), (15, 14), (16, 15), (17, 1), (18, 17), (19, 18), (20, 19), (22, 23), (21, 21), (23, 8), (24, 25),
-        (25, 12)
-    ),
-    'ntu/xsub': (
-        (1, 2), (2, 21), (3, 21), (4, 3), (5, 21), (6, 5), (7, 6), (8, 7), (9, 21), (10, 9), (11, 10), (12, 11),
-        (13, 1),
-        (14, 13), (15, 14), (16, 15), (17, 1), (18, 17), (19, 18), (20, 19), (22, 23), (21, 21), (23, 8), (24, 25),
-        (25, 12)
-    ),
-
-    'kinetics': ((0, 0), (1, 0), (2, 1), (3, 2), (4, 3), (5, 1), (6, 5), (7, 6), (8, 2), (9, 8), (10, 9),
-                 (11, 5), (12, 11), (13, 12), (14, 0), (15, 0), (16, 14), (17, 15))
-}
-
-sets = {
-    'train', 'val'
-}
-
-# 'ntu/xview', 'ntu/xsub',  'kinetics'
-datasets = {
-    'ntu/xview', 'ntu/xsub',
-}
-# bone
 from tqdm import tqdm
 
-for dataset in datasets:
-    for set in sets:
-        print(dataset, set)
-        data = np.load('../data/{}/{}_data_joint.npy'.format(dataset, set))
-        N, C, T, V, M = data.shape
-        fp_sp = open_memmap(
-            '../data/{}/{}_data_bone.npy'.format(dataset, set),
-            dtype='float32',
-            mode='w+',
-            shape=(N, 3, T, V, M))
+paris = {
+    'ntu/xview': (
+        (1, 2), (2, 21), (3, 21), (4, 3), (5, 21),
+        (6, 5), (7, 6), (8, 7), (9, 21), (10, 9),
+        (11, 10), (12, 11), (13, 1), (14, 13), (15, 14),
+        (16, 15), (17, 1), (18, 17), (19, 18), (20, 19),
+        (22, 23), (21, 21), (23, 8), (24, 25), (25, 12)
+    ),
+    'ntu/xsub': (
+        (1, 2), (2, 21), (3, 21), (4, 3), (5, 21),
+        (6, 5), (7, 6), (8, 7), (9, 21), (10, 9),
+        (11, 10), (12, 11), (13, 1), (14, 13), (15, 14),
+        (16, 15), (17, 1), (18, 17), (19, 18), (20, 19),
+        (22, 23), (21, 21), (23, 8), (24, 25), (25, 12)
+    ),
 
-        fp_sp[:, :C, :, :, :] = data
-        for v1, v2 in tqdm(paris[dataset]):
-            if dataset != 'kinetics':
-                v1 -= 1
-                v2 -= 1
-            fp_sp[:, :, :, v1, :] = data[:, :, :, v1, :] - data[:, :, :, v2, :]
+    'kinetics': (
+        (0, 0), (1, 0), (2, 1), (3, 2), (4, 3), (5, 1),
+        (6, 5), (7, 6), (8, 2), (9, 8), (10, 9), (11, 5),
+        (12, 11), (13, 12), (14, 0), (15, 0), (16, 14), (17, 15)
+    )
+}
+
+sets = {'train', 'val'}
+datasets = {'ntu/xview', 'ntu/xsub'}
+
+if __name__ == '__main__':
+    for dataset in datasets:
+        for set in sets:
+            print(dataset, set)
+            data = np.load('../data/{}/{}_data_joint.npy'.format(dataset, set))
+            N, C, T, V, M = data.shape
+            fp_sp = open_memmap(
+                '../data/{}/{}_data_bone.npy'.format(dataset, set),
+                dtype='float32',
+                mode='w+',
+                shape=(N, 3, T, V, M))
+
+            # Copy the joints data to bone placeholder tensor
+            fp_sp[:, :C, :, :, :] = data
+            for v1, v2 in tqdm(paris[dataset]):
+                # Reduce class index for NTU datasets
+                if dataset != 'kinetics':
+                    v1 -= 1
+                    v2 -= 1
+                # Assign bones to be joint1 - joint2
+                # There also happens to be 25 bones
+                fp_sp[:, :, :, v1, :] = data[:, :, :, v1, :] - data[:, :, :, v2, :]
--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -1,3 +1,4 @@
+import re
 import argparse
 import pickle
 from tqdm import tqdm
@@ -6,10 +7,11 @@ import sys
 sys.path.extend(['../'])
 from data_gen.preprocess import pre_normalization
 
-training_subjects = [
-    1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38
-]
+# For Cross-Subject benchmark "xsub"
+training_subjects = [1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38]
+# For Cross-View benchmark "xview"
 training_cameras = [2, 3]
+
 max_body_true = 2
 max_body_kinect = 4
 num_joint = 25
@@ -60,11 +62,13 @@ def read_skeleton_filter(file):
     return skeleton_sequence
 
 
-def get_nonzero_std(s):  # tvc
-    index = s.sum(-1).sum(-1) != 0  # select valid frames
-    s = s[index]
+def get_nonzero_std(s):
+    # `s` has shape (T, V, C)
+    # Select valid frames where sum of all nodes is nonzero
+    s = s[s.sum((1,2)) != 0]
     if len(s) != 0:
-        s = s[:, :, 0].std() + s[:, :, 1].std() + s[:, :, 2].std()  # three channels
+        # Compute sum of standard deviation for all 3 channels as `energy`
+        s = s[..., 0].std() + s[..., 1].std() + s[..., 2].std()
     else:
         s = 0
     return s
@@ -72,20 +76,19 @@ def get_nonzero_std(s):  # tvc
 
 def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
     seq_info = read_skeleton_filter(file)
+    # Create data tensor of shape: (# persons (M), # frames (T), # nodes (V), # channels (C))
     data = np.zeros((max_body, seq_info['numFrame'], num_joint, 3))
     for n, f in enumerate(seq_info['frameInfo']):
         for m, b in enumerate(f['bodyInfo']):
             for j, v in enumerate(b['jointInfo']):
                 if m < max_body and j < num_joint:
                     data[m, n, j, :] = [v['x'], v['y'], v['z']]
-                else:
-                    pass
 
-    # select two max energy body
+    # select 2 max energy body
     energy = np.array([get_nonzero_std(x) for x in data])
     index = energy.argsort()[::-1][0:max_body_true]
     data = data[index]
-
+    # Data new shape: (C, T, V, M)
     data = data.transpose(3, 1, 2, 0)
     return data
 
@@ -93,36 +96,32 @@ def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
 def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', part='eval'):
     if ignored_sample_path != None:
         with open(ignored_sample_path, 'r') as f:
-            ignored_samples = [
-                line.strip() + '.skeleton' for line in f.readlines()
-            ]
+            ignored_samples = [line.strip() + '.skeleton' for line in f.readlines()]
     else:
         ignored_samples = []
+
     sample_name = []
     sample_label = []
     for filename in os.listdir(data_path):
         if filename in ignored_samples:
             continue
-        action_class = int(
-            filename[filename.find('A') + 1:filename.find('A') + 4])
-        subject_id = int(
-            filename[filename.find('P') + 1:filename.find('P') + 4])
-        camera_id = int(
-            filename[filename.find('C') + 1:filename.find('C') + 4])
+        action_class = int(filename[filename.find('A') + 1:filename.find('A') + 4])
+        subject_id = int(filename[filename.find('P') + 1:filename.find('P') + 4])
+        camera_id = int(filename[filename.find('C') + 1:filename.find('C') + 4])
 
         if benchmark == 'xview':
             istraining = (camera_id in training_cameras)
         elif benchmark == 'xsub':
             istraining = (subject_id in training_subjects)
         else:
-            raise ValueError()
+            raise ValueError('Invalid benchmark provided: {}'.format(benchmark))
 
         if part == 'train':
             issample = istraining
         elif part == 'val':
             issample = not (istraining)
         else:
-            raise ValueError()
+            raise ValueError('Invalid data part provided: {}'.format(part))
 
         if issample:
             sample_name.append(filename)
@@ -131,12 +130,13 @@ def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', pa
     with open('{}/{}_label.pkl'.format(out_path, part), 'wb') as f:
         pickle.dump((sample_name, list(sample_label)), f)
 
+    # Create data tensor with shape (# examples (N), C, T, V, M)
     fp = np.zeros((len(sample_label), 3, max_frame, num_joint, max_body_true), dtype=np.float32)
 
     # Fill in the data tensor `fp` one training example a time
     for i, s in enumerate(tqdm(sample_name)):
         data = read_xyz(os.path.join(data_path, s), max_body=max_body_kinect, num_joint=num_joint)
-        fp[i, :, 0:data.shape[1], :, :] = data
+        fp[i, :, :data.shape[1], :, :] = data
 
     fp = pre_normalization(fp)
     np.save('{}/{}_data_joint.npy'.format(out_path, part), fp)
@@ -149,12 +149,12 @@ if __name__ == '__main__':
                         default='../data/nturgbd_raw/samples_with_missing_skeletons.txt')
     parser.add_argument('--out_folder', default='../data/ntu/')
 
-    benchmark = ['xsub', 'xview']
-    part = ['train', 'val']
+    benchmarks = ['xsub', 'xview']
+    parts = ['train', 'val']
     arg = parser.parse_args()
 
-    for b in benchmark:
-        for p in part:
+    for b in benchmarks:
+        for p in parts:
             out_path = os.path.join(arg.out_folder, b)
             if not os.path.exists(out_path):
                 os.makedirs(out_path)
