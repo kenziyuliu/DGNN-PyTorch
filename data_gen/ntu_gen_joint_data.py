140b0f32721cf540b2acc738e5f0a98d9040398d
changyun <cyun9601@gmail.com>
2021-07-06 16:58:27 +0900

    add node mask and action selector

--- data_gen/ntu_gen_joint_data.py
+++ data_gen/ntu_gen_joint_data.py
@@ -3,11 +3,12 @@ import re
 import pickle
 import argparse
 import numpy as np
+import yaml
 
 from tqdm import tqdm
 import sys
 sys.path.extend(['../'])
-from data_gen.preprocess import pre_normalization
+from preprocess import pre_normalization
 
 # For Cross-Subject benchmark "xsub"
 training_subjects = [1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38]
@@ -72,8 +73,8 @@ def get_nonzero_std(s):
         s = 0
     return s
 
-
-def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
+# 관절의 x,y,z 읽어오기
+def read_xyz(file, max_body=4, num_joint=25): 
     seq_info = read_skeleton_filter(file)
     # Create data tensor of shape: (# persons (M), # frames (T), # nodes (V), # channels (C))
     data = np.zeros((max_body, seq_info['numFrame'], num_joint, 3))
@@ -87,34 +88,53 @@ def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
     energy = np.array([get_nonzero_std(x) for x in data])
     index = energy.argsort()[::-1][0:max_body_true]
     data = data[index]
-    # Data new shape: (C, T, V, M)
+
+    # Data new shape: (C, T, V, M) (# channels (C), # frames (T), # nodes (V), # persons (M))
     data = data.transpose(3, 1, 2, 0)
     return data
 
 
-def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', part='eval'):
+def gendata(data_path, out_path, node_mask=None, attention_action=None, ignored_sample_path=None, 
+            benchmark='xview', part='eval'):
+    
+    ########################################### 데이터 선별 ###########################################
+    # attention action, ignored_sample_path, benchmark, part에 따라 Train 데이터와 Test 데이터를 선별 
+    # 선별된 데이터의 파일 이름과 Label을 List로 저장
+    ###################################################################################################
+    # ignored_sample_path
     if ignored_sample_path != None:
         with open(ignored_sample_path, 'r') as f:
             ignored_samples = [line.strip() + '.skeleton' for line in f.readlines()]
     else:
         ignored_samples = []
 
-    sample_name = []
-    sample_label = []
+    sample_name = [] # benchmark와 part에 따라 선택되는 데이터 파일의 이름을 저장하는 List
+    sample_label = [] # benchmark와 part에 따라 선택되는 데이터 파일의 라벨을 저장하는 List
+    
+    # Label data 생성
     for filename in os.listdir(data_path):
+        # ignored_sample_path가 입력으로 들어오면 그 데이터는 Pass
         if filename in ignored_samples:
             continue
+        
+        # attention_action에 입력이 있으면 그 action class만 데이터 생성
+        if attention_action != None :
+            if filename[filename.find('A'):filename.find('A') + 4] not in attention_action :
+                continue
+        
         action_class = int(filename[filename.find('A') + 1:filename.find('A') + 4])
         subject_id = int(filename[filename.find('P') + 1:filename.find('P') + 4])
         camera_id = int(filename[filename.find('C') + 1:filename.find('C') + 4])
 
+        # View와 Sub의 Train 데이터셋 분리하는 부분
         if benchmark == 'xview':
-            istraining = (camera_id in training_cameras)
+            istraining = (camera_id in training_cameras) # istraining은 bool type
         elif benchmark == 'xsub':
-            istraining = (subject_id in training_subjects)
+            istraining = (subject_id in training_subjects) # istraining은 bool type
         else:
             raise ValueError('Invalid benchmark provided: {}'.format(benchmark))
 
+        # Train과 Val에 따라 데이터 선택
         if part == 'train':
             issample = istraining
         elif part == 'val':
@@ -122,35 +142,90 @@ def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', pa
         else:
             raise ValueError('Invalid data part provided: {}'.format(part))
 
-        if issample:
+        # 선택된 Sample들의 Filename과 라벨 데이터를 list로 저장
+        if issample: 
             sample_name.append(filename)
-            sample_label.append(action_class - 1)
-
+            
+            # attention action이 있으면, 라벨들이 앞으로 당겨지게끔 설정
+            if attention_action != None :
+                sample_label.append(attention_action.index(filename[filename.find('A'):filename.find('A') + 4]))
+            
+            # attention action이 없으면, filename 안의 Action number가 라벨
+            else:
+                sample_label.append(action_class - 1)
+                
+    # 선택된 Sample들의 이름과 라벨 저장
     with open('{}/{}_label.pkl'.format(out_path, part), 'wb') as f:
         pickle.dump((sample_name, list(sample_label)), f)
 
-    # Create data tensor with shape (# examples (N), C, T, V, M)
+    ########################################## 데이터 전처리 ##########################################
+    # 위에서 선별된 File name에 맞는 데이터를 Load하여 N, C, T, V, M의 joint 데이터를 Load
+    # Joint data Normalize
+    # node_mask로 선택한 node만 npy 파일로 데이터 저장
+    ###################################################################################################
+    # Joint data 생성    
+    # Joint data를 저장할 임시 변수 생성. tensor with shape (# examples (N), C=3, T=300, V=25, M=2)
     fp = np.zeros((len(sample_label), 3, max_frame, num_joint, max_body_true), dtype=np.float32)
 
-    # Fill in the data tensor `fp` one training example a time
+    # Sample File에서 Joint Data를 읽어와서 임시 변수(fp)에 저장
     for i, s in enumerate(tqdm(sample_name)):
-        data = read_xyz(os.path.join(data_path, s), max_body=max_body_kinect, num_joint=num_joint)
-        fp[i, :, :data.shape[1], :, :] = data
-
-    fp = pre_normalization(fp)
+        # Data new shape: (C, T, V, M)
+        data = read_xyz(os.path.join(data_path, s), max_body=max_body_kinect, num_joint=num_joint) # (C, T, V, M)
+        fp[i, :, :data.shape[1], :, :] = data # (N, C, T, V, M)
+    
+    # fp normalize
+    fp = pre_normalization(fp) # (N, C=3, T=300, V=25, M=2)
+    
+    
+    # 특정 노드만 가져오기
+    if node_mask != None : 
+        node_mask = node_mask - np.ones([len(node_mask)], dtype=int)
+        fp = fp[:, :, :, node_mask, :]
+    
+    # Joint data 저장
     np.save('{}/{}_data_joint.npy'.format(out_path, part), fp)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
-    parser.add_argument('--data_path', default='../data/nturgbd_raw/nturgb+d_skeletons/')
+    parser.add_argument('--data_path', default='/mnt/disk2/data/private_data/NTU_RGB+D/nturgb+d_skeletons/nturgbd_skeletons_s001_to_s017/')
     parser.add_argument('--ignored_sample_path',
-                        default='../data/nturgbd_raw/samples_with_missing_skeletons.txt')
-    parser.add_argument('--out_folder', default='../data/ntu/')
+                        default='./data/nturgbd_raw/samples_with_missing_skeletons.txt')
+    parser.add_argument('--out_folder', default='./data/ntu/')
+    parser.add_argument(
+            '--node_mask',
+            default=None,
+            help='사용할 노드') # 사용할 노드 
+    parser.add_argument(
+            '--selected_action',
+            default=None,
+            help='action to train') # 사용할 Action 번호
+    parser.add_argument(
+            '--config',
+            default='./config/node_config.yaml',
+            help='path to the configuration file') # config 파일이 위치하는 디렉토리
 
     benchmarks = ['xsub', 'xview']
     parts = ['train', 'val']
     arg = parser.parse_args()
+
+    if arg.config is not None: 
+        with open(arg.config, 'r') as f:
+            default_arg = yaml.load(f) # config 파일에 들어있는 keys. config key로 명명
+ 
+        # 예외 처리 
+        key = vars(arg).keys() # parser에 들어있는 key값. default key로 명명
+        for k in default_arg.keys():
+            if k not in key: # config key가 default key에 들어있지 않으면
+                print('WRONG ARG: {}'.format(k)) # default key에 해당 키가 없음을 알림.
+                assert (k in key)
+
+        parser.set_defaults(**default_arg) # 임의의 개수의 Keyword arguments를 받아서 default key -> config key로 변경
+
+    arg = parser.parse_args() # config key가 반영된 argument
+
+    print("arg : ", arg)
 
     for b in benchmarks:
         for p in parts:
@@ -159,8 +234,10 @@ if __name__ == '__main__':
                 os.makedirs(out_path)
             print(b, p)
             gendata(
-                arg.data_path,
-                out_path,
-                arg.ignored_sample_path,
-                benchmark=b,
-                part=p)
+                arg.data_path, # 전처리할 Skeleton data가 위치한 경로. NTU RGB 데이터를 의미
+                out_path, # 전처리된 데이터가 위치할 공통 경로.
+                node_mask=arg.node_mask, # node mask. 25개의 Joint를 다 사용하지 않을 경우 사용할 Joint를 List 형태로 넣으면 된다. 
+                attention_action = arg.selected_action, # 특정 Action에 대해서만 데이터 생성. 원하는 Action을 List 형태로 넣으면 된다.
+                ignored_sample_path = arg.ignored_sample_path, # 무시할 Sample 데이터가 적혀진 txt파일의 경로. 
+                benchmark=b, # xsub / xview
+                part=p) # train할지, validation할지 
--- data_gen/preprocess.py
+++ data_gen/preprocess.py
@@ -2,62 +2,98 @@ import sys
 sys.path.extend(['../'])
 
 import numpy as np
-from data_gen.rotation import angle_between, rotation_matrix
+from rotation import angle_between, rotation_matrix
 from tqdm import tqdm
 
-
 def pre_normalization(data, zaxis=[0, 1], xaxis=[8, 4]):
+    # examples (N), channels (C), frames (T), nodes (V), persons (M))
     N, C, T, V, M = data.shape
     s = np.transpose(data, [0, 4, 2, 3, 1])  # to (N, M, T, V, C)
-
+    
+    ###################################################################################################################
+    # 1. 스켈레톤 데이터에서 중간 Frame부터 동작이 있는 경우 데이터 Shift
+    # 2. 스켈레톤 데이터에서 뒷부분에 동작이 없는 경우, 유효한 데이터를 여러번 반복
     print('pad the null frames with the previous frames')
-    for i_s, skeleton in enumerate(tqdm(s)):  # Dimension N
-        if skeleton.sum() == 0:
+    for i_s, skeleton in enumerate(tqdm(s)):  # Dimension N   # skeleton : (M, T, V, C) (2, 300, 25, 3)
+        
+        # skeleton 데이터가 전부 0으로 되어 있는 데이터 찾기. - 잘못된 데이터 검출 
+        # continue가 빠진 것 같음
+        if skeleton.sum() == 0:  
             print(i_s, ' has no skeleton')
-        for i_p, person in enumerate(skeleton): # Dimension M (# person)
-            # `person` has shape (T, V, C)
-            if person.sum() == 0:
+        
+        # 전체 스켈레톤 데이터에 대해
+        for i_p, person in enumerate(skeleton): # Dimension M (# person)  # person : (T, V, C) (300, 25, 3)
+            # Person 데이터가 전부 0인 경우(Person 1과 Person 2가 전부 0)이면 넘기기
+            if person.sum() == 0: # person 
                 continue
+            
+            # 1. Person의 첫번째 프레임이 0인 경우. 즉, 프레임 초반에는 데이터가 없는 경우.      
             if person[0].sum() == 0:
                 # `index` of frames that have non-zero nodes
+                # frames의 합 != 0인 인덱스 찾기. 즉, T가 30일 때, Frame이 5부터 10까지인 데이터는 5~10의 인덱스 반환.
                 index = (person.sum(-1).sum(-1) != 0)
-                tmp = person[index].copy()
+                tmp = person[index].copy() # 프레임 임시 저장
                 # Shift non-zero nodes to beginning of frames
                 person *= 0
-                person[:len(tmp)] = tmp
-            for i_f, frame in enumerate(person):
-                # Each frame has shape (V, C)
+                person[:len(tmp)] = tmp # shift
+            
+            # 전체 스켈레톤 데이터의 Node와 Channel에 대해
+            for i_f, frame in enumerate(person): # Each frame : (V, C)
+                # 한 프레임에 대해서 데이터가 0이면(스켈레톤 데이터가 없으면)
                 if frame.sum() == 0:
+                    # 그 프레임의 데이터부터 끝까지 데이터가 0이면(스켈레톤 데이터가 없으면)
                     if person[i_f:].sum() == 0:
-                        # Repeat all the frames up to now (`i_f`) till the max seq len
-                        rest = len(person) - i_f
-                        reps = int(np.ceil(rest / i_f))
-                        pad = np.concatenate([person[:i_f] for _ in range(reps)], 0)[:rest]
+                        # 2. Repeat all the frames up to now (`i_f`) till the max seq len
+                        rest = len(person) - i_f # 나머지 프레임 갯수
+                        reps = int(np.ceil(rest / i_f)) # 같은 동작을 몇번 반복할 것인지.
+                        pad = np.concatenate([person[:i_f] for _ in range(reps)], 0)[:rest] # 프레임 수가 넘어가면 뒷부분 자르기
                         s[i_s, i_p, i_f:] = pad
                         break
-
-    print('sub the center joint #1 (spine joint in ntu and neck joint in kinetics)')
+    ###################################################################################################################
+    
+    # Joint 데이터를 x, y, z = 0, 0, 0 좌표 근처로 이동
+    print('subtract the center joint #1 (spine joint in ntu and neck joint in kinetics)')
     for i_s, skeleton in enumerate(tqdm(s)):
+        # Person 데이터가 전부 0인 경우(Person 1과 Person 2가 전부 0)이면 넘기기
         if skeleton.sum() == 0:
             continue
-        # Use the first skeleton's body center (`1:2` along the nodes dimension)
-        main_body_center = skeleton[0][:, 1:2, :].copy()    # Shape (T, 1, C)
-        for i_p, person in enumerate(skeleton):
-            if person.sum() == 0:
+            
+        # Use the first skeleton's body center (`1:2` along the nodes dimension)  
+        # 첫번째 Person의 복부 부분의 Joint를 Main body center로 저장(2번 Joint)
+        '''
+        이상한 게 첫번째 프레임의 2번 Joint 좌표를 중심으로 보는 것이 아니라
+        모든 프레임에서 2번 Joint 좌표를 중심으로 바라봄.
+        이 경우, 달리기는 제자리기 뛰기가 되는 단점이 존재할 것 같음.
+        '''
+        
+        main_body_center = skeleton[0][:, 1:2, :].copy() # skeleton: (M, T, V, C) (2, 300, 25, 3) -> main body center: (T, 1, C)
+        
+        # 전체 스켈레톤에 대해
+        for i_p, person in enumerate(skeleton): # Dimension M (# person)  # person : (T, V, C)
+            
+            # Person 데이터가 전부 0인 경우(Person 1과 Person 2가 전부 0)이면 넘기기(continue)
+            if person.sum() == 0: # person : (T, V, C)  frames (T), nodes (V), channels (C)
                 continue
+                
             # For all `person`, compute the `mask` which is the non-zero channel dimension
-            mask = (person.sum(-1) != 0).reshape(T, V, 1)
+            # Person 데이터가 존재하는 경우, mask는 X, Y, Z 값의 합이 0이 아니면(Action이 있으면) True, 0이면 False
+            mask = (person.sum(-1) != 0).reshape(T, V, 1) 
+            
             # Subtract the first skeleton's centre joint
+            # Skeleton data의 2번째 Joint를 모두 0, 0, 0으로 이동시킴.
             s[i_s, i_p] = (s[i_s, i_p] - main_body_center) * mask
 
+    # 척추 부분을 Z축과 Parallel하게 만들음.
     print('parallel the bone between hip(jpt 0) and spine(jpt 1) of the first person to the z axis')
-    for i_s, skeleton in enumerate(tqdm(s)):
+    for i_s, skeleton in enumerate(tqdm(s)): # skeleton: (M, T, V, C) (2, 300, 25, 3) # persons (M), frames (T), nodes (V), channels (C) 
+        # Person 데이터가 전부 0인 경우(Person 1과 Person 2가 전부 0)이면 넘기기
         if skeleton.sum() == 0:
             continue
+        
         # Shapes: (C,)
-        joint_bottom = skeleton[0, 0, zaxis[0]]
-        joint_top = skeleton[0, 0, zaxis[1]]
-        axis = np.cross(joint_top - joint_bottom, [0, 0, 1])
+        joint_bottom = skeleton[0, 0, zaxis[0]] # zxais = [0, 1]이면, 1번 Joint  
+        joint_top = skeleton[0, 0, zaxis[1]] # zxais = [0, 1]이면, 2번 Joint  
+        axis = np.cross(joint_top - joint_bottom, [0, 0, 1]) # bottom to top vector. XY평면이 지평면
         angle = angle_between(joint_top - joint_bottom, [0, 0, 1])
         matrix_z = rotation_matrix(axis, angle)
         for i_p, person in enumerate(skeleton):
@@ -69,11 +105,12 @@ def pre_normalization(data, zaxis=[0, 1], xaxis=[8, 4]):
                 for i_j, joint in enumerate(frame):
                     s[i_s, i_p, i_f, i_j] = np.dot(matrix_z, joint)
 
+    # 양쪽 어깨선을 X축과 나란하게 만들음.
     print('parallel the bone between right shoulder(jpt 8) and left shoulder(jpt 4) of the first person to the x axis')
     for i_s, skeleton in enumerate(tqdm(s)):
         if skeleton.sum() == 0:
             continue
-        joint_rshoulder = skeleton[0, 0, xaxis[0]]
+        joint_rshoulder = skeleton[0, 0, xaxis[0]] # xaxis = [8, 4]
         joint_lshoulder = skeleton[0, 0, xaxis[1]]
         axis = np.cross(joint_rshoulder - joint_lshoulder, [1, 0, 0])
         angle = angle_between(joint_rshoulder - joint_lshoulder, [1, 0, 0])
@@ -89,7 +126,6 @@ def pre_normalization(data, zaxis=[0, 1], xaxis=[8, 4]):
 
     data = np.transpose(s, [0, 4, 2, 3, 1])
     return data
-
 
 if __name__ == '__main__':
     data = np.load('../data/ntu/xview/val_data.npy')
